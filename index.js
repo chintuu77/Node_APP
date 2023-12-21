6f246ff9345762a35fcfd3b50a7aeb29bdaa88f4
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2023-12-21 19:15:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello APPS DEPLOYER This is Gouserabbani Shaik!\n'
-  const msg = 'Hello APPS DEPLOYER This is Vivek \n'
+  const msg = 'Hello APPS DEPLOYER This is Gouserabbani Shaik! vivek\n'
   res.end(msg);
 });
 
